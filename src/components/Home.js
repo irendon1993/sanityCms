f086d76e1593c308aa1f2371d7eb8f666815d2ab
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2021-01-07 08:36:28 -0500

    Started working on Home page and blog post

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React from "react"
+import BackgroundImage from "../images/ImageLaserEngraving.jpg"
 
 export default function Home() {
     return (
         <main>
-            <img />
-            <section>
-                <h1>Quality Engineer Solutions</h1>
+            <img 
+            src={BackgroundImage}
+            alt="Laser Ingraving"
+            className="absolute object-cover w-full h-full"
+            />
+            <section className="relative flex justify-center min-h-screen p-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Quality Engineer Solutions</h1>
             </section>
         </main>
     )
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,48 @@
-import React from "react"
+import { findAllByTitle } from "@testing-library/react";
+import React, {useState, useEffect } from "react";
+import sanityClient from "../client.js";
 
 export default function Post() {
-    return <h1>Post Page!</h1>
+    const [postData, setPost] =useState(null);
     
+    useEffect(() => {
+        sanityClient
+            .fetch(
+                `*[_type == "post"]{
+                    title,
+                    slug,
+                    mainImage{
+                        asset->{
+                            _id,
+                            url 
+                        },
+                        alt
+                    }
+                }`
+            )
+            .then((data => setPost(data))
+            .catch(console.error);
+    }, []);
+    
+    return (
+        <main className="bg-green-100 min-h-screen p-12">
+            <section className="container mx-auto">
+                <h1 className="text-5xl flex justify-center cursive">Blog Post Page</h1>
+                <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page of blog posts</h2>
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    <article>
+                        <Link>
+                        <span>
+                            <img />
+                            <span>
+                                <h3></h3>
+                            </span>
+                        </span>
+                        </Link>
+                    </article>
+                </div>
+            </section>
+        </main>
+    )
+
 }
