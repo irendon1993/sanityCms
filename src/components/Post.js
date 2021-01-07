c094fe4d346c9b23a6c8afb9dc2ddf12f4f781d2
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2021-01-07 10:39:42 -0500

    Trying to fix post bug

--- src/components/Post.js
+++ src/components/Post.js
@@ -29,16 +29,16 @@ export default function Post() {
                 <h1 className="text-5xl flex justify-center cursive">Blog Post Page</h1>
                 <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page of blog posts</h2>
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {postData && postData.map((post,index) => (
+                    {postData && postData.map((post,index)=> (
                     <article>
-                        <Link to={"/post/" + postData.slug.current} key={postData.slug.current}>
+                        <Link to={"/post/" + post.slug.current} key={post.slug.current}>
                         <span
                             className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
                             key={index}
                         >
                             <img
-                            src ={postData.mainImage.asset.url}
-                            alt={postData.mainImage.alt}
+                            src ={post.mainImage.asset.url}
+                            alt={post.mainImage.alt}
                             className="w-full h-full rounded-r object-cover absolute" 
                             />
                             <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
